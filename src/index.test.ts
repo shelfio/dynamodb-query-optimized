cb048d56355d69d2ba7bc646eefa5730ef8a3bcf
Vlad Holubiev <golubev.vld@gmail.com>
2022-01-04 21:43:08 +0200

    test: cover regular query with tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,19 +1,22 @@
 import {insertMany} from './helpers/insert-many';
 import {deleteAll} from './helpers/delete-all';
 import {ddb} from './helpers/ddb';
-import {queryOptimized} from './';
+import {queryOptimized, queryRegular} from './';
 
 const hash_key = 'some-hash-key';
 const range_key = 'some-range-key';
 const getRangeKey = (range: number) => `${range_key}-${range}`;
 jest.setTimeout(120000);
 
-beforeEach(async () => {
+beforeAll(async () => {
   const listToInsert = [];
 
-  for (let i = 0; i < 3000; i++) {
+  // Each item is approx. 3.6 KB
+  // We insert approx. 10.5 MB of data with hash_key: 'some-hash-key'
+  // and ~0.9 Mb of data with hash_key: 'some-hash-key-1mb'
+  for (let i = 0; i < 3250; i++) {
     listToInsert.push({
-      hash_key,
+      hash_key: i >= 3000 ? 'some-hash-key-1mb' : hash_key,
       range_key: getRangeKey(i),
       name: 'hello',
       description: 'hello world',
@@ -29,8 +32,39 @@ beforeEach(async () => {
   });
 });
 
-it(`should return all elements using optimized find query`, async () => {
-  const result = await queryOptimized({
+it(`should return all elements using optimized find query for 10 MB table`, async () => {
+  const result = await testQueryOptimized('some-hash-key');
+
+  expect(result).toHaveLength(3000);
+});
+
+it(`should return all elements using regular find query for 10 MB table`, async () => {
+  const result = await testQueryRegular('some-hash-key');
+
+  expect(result).toHaveLength(3000);
+});
+
+it(`should return all elements using optimized find query for 1 MB table`, async () => {
+  const result = await testQueryOptimized('some-hash-key-1mb');
+
+  expect(result).toHaveLength(250);
+});
+
+it(`should return all elements using regular find query for 1 MB table`, async () => {
+  const result = await testQueryRegular('some-hash-key-1mb');
+
+  expect(result).toHaveLength(250);
+});
+
+afterAll(async () => {
+  await deleteAll({
+    TableName: 'example_table',
+    ProjectionExpression: 'hash_key, range_key',
+  });
+});
+
+async function testQueryRegular(hash_key: string) {
+  return queryRegular({
     queryFunction: ddb.query.bind(ddb),
     queryParams: {
       TableName: 'example_table',
@@ -46,16 +80,23 @@ it(`should return all elements using optimized find query`, async () => {
       },
     },
   });
+}
 
-  expect(result).toHaveLength(3000);
-});
-
-// normally, I'd write a test comparing performance of regular vs optimized find
-// but local ddb works different than the real one. findOptimized is slower locally, but ~50% faster when querying the real DB
-
-afterEach(async () => {
-  await deleteAll({
-    TableName: 'example_table',
-    ProjectionExpression: 'hash_key, range_key',
+async function testQueryOptimized(hash_key: string) {
+  return queryOptimized({
+    queryFunction: ddb.query.bind(ddb),
+    queryParams: {
+      TableName: 'example_table',
+      ProjectionExpression: 'hash_key, range_key',
+      KeyConditionExpression: '#hash_key = :hash_key AND begins_with(#range_key, :range_key)',
+      ExpressionAttributeNames: {
+        '#hash_key': 'hash_key',
+        '#range_key': 'range_key',
+      },
+      ExpressionAttributeValues: {
+        ':hash_key': hash_key,
+        ':range_key': range_key,
+      },
+    },
   });
-});
+}
